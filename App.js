33203e42ded4e5064944996eb7bcd06c6c076985
Sander <sanderjverhoeven@hotmail.com>
2022-06-03 21:16:04 +0200

    added markers

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Map from './screens/Map.js';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 // https://coolors.co/palette/d9ed92-b5e48c-99d98c-76c893-52b69a-34a0a4-168aad-1a759f-1e6091-184e77
+// https://coolors.co/palette/dad7cd-a3b18a-588157-3a5a40-344e41
 const Tab = createBottomTabNavigator();
 
 export default function App() {
--- screens/Map.js
+++ screens/Map.js
@@ -1,10 +1,39 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Image } from 'react-native';
+import markerData from '../markers.json';
+import { Marker } from 'react-native-maps';
 
 export default function MapScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Map!</Text>
-      </View>
-    );
-  }
+  
+  return (
+    <View style={styles.container}>
+      <MapView 
+        region={{
+          latitude: markerData[1].latitude,
+          longitude: markerData[1].longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map} 
+      >
+      {markerData.map((marker, index) => (
+              <Marker coordinate={{ latitude : marker.latitude , longitude : marker.longitude, }} icon={require('../assets/Marker_soju.png')}/>
+      ))}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
